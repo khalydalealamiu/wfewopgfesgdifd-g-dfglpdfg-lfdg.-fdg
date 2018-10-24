2777cd7011d691e95ca0024b98561c4ef77f3f1f
khalydalealamiu <44276417+khalydalealamiu@users.noreply.github.com>
2018-10-25 00:59:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -593,3 +593,19 @@ client.on('message',message =>{
       if (!channel) return;
       channel.sendEmbed(embed);
 });
+ client.on('message', msg => {
+    if (msg.content.startsWith(`^warn`)) {
+       let args = msg.content.split(" ").slice(1);
+      if (!msg.mentions.members.first()) return msg.reply('منشن الشخص المحدد')
+      if (!args[1]) return msg.reply('``اكتب السبب``')
+      //غير اسم الروم او سوي روم بذا الاسم
+      if (msg.guild.channels.find('name', 'warns')) {
+        //اذا غيرت فوق غير هنا كمان
+        msg.guild.channels.find('name', 'warns').send(`
+      تم اعطائك تنبيه : ${msg.mentions.members.first()}
+      لأنك قمت بما يلي
+      ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
+      `)
+      }
+    }
+})
